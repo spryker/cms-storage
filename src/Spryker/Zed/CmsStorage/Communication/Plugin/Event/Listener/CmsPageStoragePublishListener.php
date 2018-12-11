75c9d4b4f2f5dbaf651cede52bc7c3dd6e662b4b
Aleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2018-12-11 13:00:42 +0000

    tech-135: listener srp violation

--- src/Spryker/Zed/CmsStorage/Communication/Plugin/Event/Listener/CmsPageStoragePublishListener.php
+++ src/Spryker/Zed/CmsStorage/Communication/Plugin/Event/Listener/CmsPageStoragePublishListener.php
@@ -7,25 +7,25 @@
 
 namespace Spryker\Zed\CmsStorage\Communication\Plugin\Event\Listener;
 
-use Orm\Zed\Url\Persistence\Map\SpyUrlTableMap;
 use Spryker\Zed\Event\Dependency\Plugin\EventBulkHandlerInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 use Spryker\Zed\PropelOrm\Business\Transaction\DatabaseTransactionHandlerTrait;
-use Spryker\Zed\Url\Dependency\UrlEvents;
 
 /**
- * @deprecated Use `\Spryker\Zed\CmsStorage\Communication\Plugin\Event\Listener\CmsPageUrlStoragePublishListener` and `\Spryker\Zed\CmsStorage\Communication\Plugin\Event\Listener\CmsPageUrlStorageUnpublishListener` instead.
- *
  * @method \Spryker\Zed\CmsStorage\Communication\CmsStorageCommunicationFactory getFactory()
  * @method \Spryker\Zed\CmsStorage\Persistence\CmsStorageQueryContainerInterface getQueryContainer()
  * @method \Spryker\Zed\CmsStorage\Business\CmsStorageFacadeInterface getFacade()
  * @method \Spryker\Zed\CmsStorage\CmsStorageConfig getConfig()
  */
-class CmsPageUrlStorageListener extends AbstractPlugin implements EventBulkHandlerInterface
+class CmsPageStoragePublishListener extends AbstractPlugin implements EventBulkHandlerInterface
 {
     use DatabaseTransactionHandlerTrait;
 
     /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
      * @param array $eventTransfers
      * @param string $eventName
      *
@@ -34,17 +34,7 @@ class CmsPageUrlStorageListener extends AbstractPlugin implements EventBulkHandl
     public function handleBulk(array $eventTransfers, $eventName)
     {
         $this->preventTransaction();
-        $cmsPageIds = $this->getFactory()->getEventBehaviorFacade()->getEventTransferForeignKeys($eventTransfers, SpyUrlTableMap::COL_FK_RESOURCE_PAGE);
-
-        if (empty($cmsPageIds)) {
-            return;
-        }
-
-        if ($eventName === UrlEvents::ENTITY_SPY_URL_DELETE) {
-            $this->getFacade()->unpublish($cmsPageIds);
-
-            return;
-        }
+        $cmsPageIds = $this->getFactory()->getEventBehaviorFacade()->getEventTransferIds($eventTransfers);
 
         $this->getFacade()->publish($cmsPageIds);
     }
